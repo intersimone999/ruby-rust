568007b27ac36fe267ac1986eb7b6512b024af66
Simone Scalabrino <s.scalabrino9@gmail.com>
2020-09-09 18:43:54 +0200

    Added requires to basic Rust

--- lib/rust.rb
+++ lib/rust.rb
@@ -1,3 +1,6 @@
+require_relative 'rust-core'
 require_relative 'rust-tests'
 require_relative 'rust-effsize'
 require_relative 'rust-descriptive'
+require_relative 'rust-plots'
+require_relative 'rust-calls'
