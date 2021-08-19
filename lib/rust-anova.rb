ff2e74596524206368c9a4024a3517aaa0e09fef
Simone Scalabrino <s.scalabrino9@gmail.com>
2021-08-19 13:20:30 +0200

    Fixed lists and improved ANOVA
    - Fixed list, now works also if lists have no names
    - Refactored ANOVA (similar to regressions) and added summary

--- lib/rust-anova.rb
+++ lib/rust-anova.rb
@@ -1,25 +1,60 @@
 require_relative 'rust-core'
 
 module Rust
-    class ANOVA
-        def initialize(data)
-            @data = data
+    class ANOVAModel < RustDatatype
+        def self.can_pull?(type, klass)
+            return type == "list" && [klass].flatten.include?("aov")
         end
         
-        def aov(formula, **options)
-            mapped = options.map { |k, v| "#{k}=#{v}" }.join(", ")
-            mapped = ", " + mapped if mapped.length > 0
-            Rust.exclusive do
-                Rust._eval("aov.model.result <- aov(#{formula.to_R} #{mapped})")
-                return Rust["aov.model.result"]
+        def self.pull_variable(variable)
+            model = Rust::List.pull_variable(variable)
+            
+            return ANOVAModel.new(model)
+        end
+        
+        def load_in_r_as(variable_name)
+            @model.load_in_r_as(variable_name)
+        end
+        
+        def self.generate(formula, data, **options)
+            mapped = ""
+            if options.size > 0
+                mapped = options.map { |k, v| "#{k}=#{v}" }.join(", ")
+                mapped = ", " + mapped
             end
+            
+            Rust.exclusive do
+                Rust["aov.data"] = data
+                Rust._eval("aov.model.result <- aov(#{formula.to_R}, data=aov.data#{mapped})")
+                result = ANOVAModel.new(Rust["aov.model.result"])
+                result.r_mirror_to("aov.model.result")
+                return result
+            end
+        end
+        
+        def initialize(model)
+            @model = model
+        end
+        
+        def model
+            @model
+        end
+        
+        def summary
+            unless @summary
+                Rust.exclusive do
+                    Rust._eval("aov.smr <- summary(#{self.r_mirror})")
+                    @summary = Rust['aov.smr']
+                end
+            end
+            
+            return @summary
         end
     end
 end
 
 module Rust::RBindings
-    def aov(formula, data=nil, **options)
-        anova = Rust::ANOVA.new(data)
-        return anova.aov(formula, **options)
+    def aov(formula, data, **options)
+        return ANOVAModel.generate(formula, data, **options)
     end
 end
--- lib/rust-core.rb
+++ lib/rust-core.rb
@@ -195,13 +195,14 @@ module Rust
         
         def self.pull_variable(variable)
             klass = Rust._pull("class(#{variable})")
-            return List.new(klass) if Rust._pull("length(names(#{variable}))") == 0
+            return List.new(klass) if Rust._pull("length(#{variable})") == 0
             
-            elements = Rust["names(#{variable})"]
+            names    = [Rust["names(#{variable})"]].flatten
+            length   = Rust["length(#{variable})"]
             
-            list = List.new(klass)
-            elements.each do |element|
-                list[element] = Rust["#{variable}$#{element}"]
+            list = List.new(klass, names)
+            for i in 0...length
+                list[i] = Rust["#{variable}[[#{i + 1}]]"]
             end
             
             return list
@@ -210,32 +211,65 @@ module Rust
         def load_in_r_as(variable_name)
             Rust._eval("#{variable_name} <- list()")
             @data.each do |key, value|
-                Rust[key] = value
+                Rust["#{variable_name}[[#{key + 1}]]"] = value
             end
         end
         
-        def initialize(klass)
+        def initialize(klass, names = [])
             @data = {}
+            @names = names
             @klass = klass
         end
         
         def [](key)
-            @data[key]
+            key = get_key(key)
+            
+            return @data[key]
         end
         alias :| :[]
         
         def []=(key, value)
-            @data[key] = value
+            key = get_key(key)
+            
+            return @data[key] = value
         end
         
         def names
-            @data.keys
+            @names
+        end
+        
+        def inspect
+            result = ""
+            values_inspected = @data.map { |k, v| [k, v.inspect.split("\n").map { |l| "  " + l }.join("\n")] }.to_h
+            max_length = [values_inspected.map { |k, v| v.split("\n").map { |line| line.length }.max }.max.to_i, 100].min
+            
+            @data.keys.each do |i|
+                result << "-" * max_length + "\n"
+                result << (@names[i] || "[[#{i}]]") + "\n"
+                result << values_inspected[i] + "\n"
+            end
+            result << "-" * max_length
+            
+            return result
+        end
+        
+        private
+        def get_key(key)
+            if key.is_a?(String)
+                new_key = @names.index(key)
+                raise ArgumentError, "Wrong key: #{key}" unless new_key
+                key = new_key
+            end
+            
+            raise ArgumentError, "The key should be either a string or an integer" unless key.is_a?(Integer)
+            
+            return key
         end
     end
     
     class DataFrame < RustDatatype
         def self.can_pull?(type, klass)
-            return klass == "data.frame"
+            return [klass].flatten.include?("data.frame")
         end
         
         def self.pull_variable(variable)
