c3623a63c90c7be3fed03018ff17eef9a9b770c9
Simone Scalabrino <s.scalabrino9@gmail.com>
2022-08-17 14:07:24 +0200

    Added some R default datasets

--- lib/rust.rb
+++ lib/rust.rb
@@ -2,3 +2,22 @@ require_relative 'rust/core'
 require_relative 'rust/models/all'
 require_relative 'rust/plots/all'
 require_relative 'rust/stats/all'
+
+module Rust
+    @@datasets = {}
+    
+    def self.toothgrowth
+        @@datasets[:ToothGrowth] = Rust.exclusive { Rust['ToothGrowth'] } unless @@datasets[:ToothGrowth]
+        return @@datasets[:ToothGrowth]
+    end
+    
+    def self.cars
+        @@datasets[:cars] = Rust.exclusive { Rust['cars'] } unless @@datasets[:cars]
+        return @@datasets[:cars]
+    end
+    
+    def self.iris
+        @@datasets[:iris] = Rust.exclusive { Rust['iris'] } unless @@datasets[:iris]
+        return @@datasets[:iris]
+    end
+end
